8d4754347545dc9114728ba0a0105a47d9ec013e
Oleksandra <sasha5l239@gmail.com>
2022-07-25 22:13:31 +0300

    task1

---
+++ js/task1.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 10; i++) {
+  if (i % 3 === 0) {
+    console.log('fizBuz');
+  }
+  if (i % 2 == 0) {
+    console.log('fiz');
+  }
+  if (i % 2 !== 0) {
+    console.log('Buz');
+  }
+}
