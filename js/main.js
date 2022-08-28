ed3d282c489943d6db9855ffcf2f11cc7232259d
Oleksandra <sasha5l239@gmail.com>
2022-08-28 16:21:14 +0300

    full hw9

--- js/main.js
+++ js/main.js
@@ -1,140 +1,227 @@
 import { items } from './items.js';
-let cardsContainer = document.getElementById('cards-container');
-function orders() {
-  return Math.round(Math.random() * (1000 - 300) + 300);
-}
-let sort = document.getElementById('open-sort');
-sort.addEventListener('click', function () {
-  document.getElementById('cards-container').style.width = '70%';
-
-  document.getElementById('sort-container').style.display = 'block';
-  document.getElementById('card-sort-container').style.display = 'flex';
-  document.getElementById('card-sort-container').style.flexDirection = 'row-reverse';
-  document.getElementById('card-sort-container').style.justifyContent = 'space-between';
-});
-
-items.map((item) => {
-  let newItem = document.createElement('div');
-
-  newItem.className = 'card';
-  newItem.id = 'card';
-  if (item.orderInfo.inStock > 0) {
-    newItem.innerHTML =
-      '<div id = "card-cont" class = "card-cont"><div class="card-img"><img  src="../img/' +
-      item.imgUrl +
-      '" /></div> <h3>' +
-      item.name +
-      '</h3> <div class="card-info"><div class="stock"><div class = "check-round"></div><p>' +
-      item.orderInfo.inStock +
-      ' left in stock</p></div><p>Prise: <b> ' +
-      item.price +
-      '</b> $</p></div><button class="addbtn">Add to cart</button></div> <div class="card-footer"><div class="col1"><img class="like" src="../img/icons/like_filled.png" /><p><b>' +
-      item.orderInfo.reviews +
-      '%</b>  Positive reviews</p><p>Above avarage</p></div><div class="col2"><p><b>' +
-      orders() +
-      '</b> orders</p></div></div></div> <div id="modal" class="modal"><button  class="close">X</button><div class="modal-container"><div class="modal-img"><img  src="../img/' +
-      item.imgUrl +
-      '" /></div><div class="modal-info"><h3>' +
-      item.name +
-      '</h3><div class="modal-footer"><div class="col01"><img class="like" src="../img/icons/like_filled.png" /><p><b>' +
-      item.orderInfo.reviews +
-      '%</b>  Positive reviews</p><p>Above avarage</p></div><div class="col02"><p><b>' +
-      orders() +
-      '</b> orders</p></div></div><div class="list">   <ul class="info-list"> <li class="info-item">Color: ' +
-      item.color +
-      ' </li> <li class="info-item">Operating System: ' +
-      item.os +
-      '</li><li class="info-item">Chip: ' +
-      item.chip.name +
-      '</li><li class="info-item">Height: ' +
-      item.size.height +
-      ' cm</li>   <li class="info-item">Width: ' +
-      item.size.width +
-      ' cm</li><li class="info-item">Depth: ' +
-      item.size.depth +
-      ' cm</li><li class="info-item">Weight: ' +
-      item.size.weight +
-      ' g</li></ul></div></div><div class="modal-price"> <h2>$ ' +
-      item.price +
-      '</h2><span class="leftInStock">Stock: <b>' +
-      item.orderInfo.inStock +
-      '</b> pcs.</span><button class="addbtn">Add to cart</button></div></div></div></div>';
-    cardsContainer.appendChild(newItem);
+///////////////////////////
+let openSort = document.getElementById('open-sort');
+openSort.addEventListener('click', function () {
+  let x = document.getElementById('sort-fields');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    newItem.innerHTML =
-      '<div class = "card-cont"><div class="card-img"><img src="../img/' +
-      item.imgUrl +
-      '" /></div> <h3>' +
-      item.name +
-      '</h3> <div class="card-info"><div class="stock"><img src="../img/icons/close.svg" /><p>' +
-      item.orderInfo.inStock +
-      ' left in stock</p></div><p>Prise: <b> ' +
-      item.price +
-      '</b> $</p></div><button class="addbtn">Add to cart</button></div><div class="card-footer"><div class="col1"><img class="like" src="../img/icons/like_filled.png" /><p><b>' +
-      item.orderInfo.reviews +
-      '%</b>  Positive reviews</p><p>  Above avarage</p></div><div class="col2"><p><b>' +
-      orders() +
-      '</b> orders</p></div></div></div><div id="modal" class="modal" > <button  class="close">X</button><div class="modal-container"><div class="modal-img"><img  src="../img/' +
-      item.imgUrl +
-      '" /></div><div class="modal-info"><h3>' +
-      item.name +
-      '</h3><div class="modal-footer"><div class="col01"><img class="like" src="../img/icons/like_filled.png" /><p><b>' +
-      item.orderInfo.reviews +
-      '%</b>  Positive reviews</p><p>Above avarage</p></div><div class="col02"><p><b>' +
-      orders() +
-      '</b> orders</p></div></div><div class="list">   <ul class="info-list"> <li class="info-item">Color: ' +
-      item.color +
-      ' </li> <li class="info-item">Operating System: ' +
-      item.os +
-      '</li><li class="info-item">Chip: ' +
-      item.chip.name +
-      '</li><li class="info-item">Height: ' +
-      item.size.height +
-      ' cm</li>   <li class="info-item">Width: ' +
-      item.size.width +
-      ' cm</li><li class="info-item">Depth: ' +
-      item.size.depth +
-      ' cm</li><li class="info-item">Weight: ' +
-      item.size.weight +
-      ' g</li></ul></div></div><div class="modal-price"> <h2>$ ' +
-      item.price +
-      '</h2><span class="leftInStock">Stock: <b>' +
-      item.orderInfo.inStock +
-      '</b> pcs.</span><button class="addbtn">Add to cart</button></div></div></div></div>';
-    cardsContainer.appendChild(newItem);
+    x.style.display = 'block';
   }
 });
-let card = document.getElementsByClassName('card');
-for (let i = 0; i < card.length; i++) {
-  card[i].onclick = function () {
-    document.getElementsByClassName('modal')[i].setAttribute('style', 'visibility:visible');
-  };
+///////////////////////////////
+let colorSort = document.getElementById('color-sort');
+let colorShow = document.getElementById('color-show');
+colorShow.addEventListener('click', function () {
+  if (colorShow.getAttribute('class') === 'arrow-right' && colorSort.style.display === 'none') {
+    colorShow.setAttribute('class', 'arrow-bottom');
+    colorSort.style.display = 'block';
+  } else {
+    colorShow.setAttribute('class', 'arrow-right');
+    colorSort.style.display = 'none';
+  }
+});
+
+let color = [];
+for (let i = 0; i < items.length; i++) {
+  color.push(items[i].color);
 }
-let close = document.getElementsByClassName('close');
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    document.getElementById('modal').removeAttribute('style');
-  };
+color = color.flat();
+color = color.filter((el, id) => color.indexOf(el) === id);
+
+let colorDiv = document.getElementById('color-sort');
+
+for (let i = 0; i < color.length; i++) {
+  let div = document.createElement('div');
+
+  let check = document.createElement('input');
+  check.type = 'checkbox';
+  check.id = `${color[i]}`;
+  let label = document.createElement('label');
+  label.setAttribute('for', `${color[i]}`);
+  label.innerText = `${color[i]}`;
+  // label.setAttribute('class', 'text-inp-dop');
+  div.appendChild(check);
+  div.appendChild(label);
+  colorDiv.appendChild(div);
 }
 
+let priceSort = document.getElementById('price-sort');
 let price = document.getElementById('price-show');
-if (price.getAttribute('class') == 'arrow-right') {
-  price.addEventListener('click', function () {
-    price.removeAttribute('class');
+price.addEventListener('click', function () {
+  if (price.getAttribute('class') === 'arrow-right' && priceSort.style.display === 'none') {
     price.setAttribute('class', 'arrow-bottom');
-    document.getElementById('sort-price').style.display = 'block';
-  });
-} else if (price.getAttribute('class') == 'arrow-bottom') {
-  price.addEventListener('click', function () {
-    price.removeAttribute('class');
+    priceSort.style.display = 'block';
+  } else {
     price.setAttribute('class', 'arrow-right');
-    document.getElementById('sort-price').style.display = 'none';
+    priceSort.style.display = 'none';
+  }
+});
+
+/////////////////////////////////////////////////
+let memorySort = document.getElementById('memory-sort');
+let memoryShow = document.getElementById('memory-show');
+memoryShow.addEventListener('click', function () {
+  if (memoryShow.getAttribute('class') === 'arrow-right' && memorySort.style.display === 'none') {
+    memoryShow.setAttribute('class', 'arrow-bottom');
+    memorySort.style.display = 'block';
+  } else {
+    memoryShow.setAttribute('class', 'arrow-right');
+    memorySort.style.display = 'none';
+  }
+});
+let memory = [];
+for (let i = 0; i < items.length; i++) {
+  memory.push(items[i].storage);
+}
+memory = memory.filter((el, id) => memory.indexOf(el) === id);
+let memoryDiv = document.getElementById('memory-sort');
+
+for (let i = 0; i < memory.length; i++) {
+  let div = document.createElement('div');
+
+  let check = document.createElement('input');
+  check.type = 'checkbox';
+  check.id = `${memory[i]}`;
+  let label = document.createElement('label');
+  label.setAttribute('for', `${memory[i]}`);
+  label.innerText = `${memory[i]} Gb`;
+  // label.setAttribute('class', 'text-inp-dop');
+  div.appendChild(check);
+  div.appendChild(label);
+  memoryDiv.appendChild(div);
+}
+///////////////////////////////////////////////////
+/////////////////////////////////////////////
+let osSort = document.getElementById('os-sort');
+let osShow = document.getElementById('os-show');
+osShow.addEventListener('click', function () {
+  if (osShow.getAttribute('class') === 'arrow-right' && osSort.style.display === 'none') {
+    osShow.setAttribute('class', 'arrow-bottom');
+    osSort.style.display = 'block';
+  } else {
+    osShow.setAttribute('class', 'arrow-right');
+    osSort.style.display = 'none';
+  }
+});
+
+let os = [];
+for (let i = 0; i < items.length; i++) {
+  os.push(items[i].os);
+}
+
+os = os.filter((el, id) => os.indexOf(el) === id);
+
+let osDiv = document.getElementById('os-sort');
+
+for (let i = 0; i < os.length; i++) {
+  let div = document.createElement('div');
+
+  let check = document.createElement('input');
+  check.type = 'checkbox';
+  check.id = `${os[i]}`;
+  let label = document.createElement('label');
+  label.setAttribute('for', `${os[i]}`);
+  label.innerText = `${os[i]}`;
+  // label.setAttribute('class', 'text-inp-dop');
+  div.appendChild(check);
+  div.appendChild(label);
+  osDiv.appendChild(div);
+}
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+let displaySort = document.getElementById('display-sort');
+let displayShow = document.getElementById('display-show');
+displayShow.addEventListener('click', function () {
+  if (displayShow.getAttribute('class') === 'arrow-right' && displaySort.style.display === 'none') {
+    displayShow.setAttribute('class', 'arrow-bottom');
+    displaySort.style.display = 'block';
+  } else {
+    displayShow.setAttribute('class', 'arrow-right');
+    displaySort.style.display = 'none';
+  }
+});
+/////////////////////////////////////////////////
+const container = document.getElementById('cards');
+
+items.forEach((element) => {
+  let text = '';
+  text += '<div class="card" id="' + element.id + '" > ';
+  text +=
+    '<div class="card-cont"><img src="./img/' +
+    element.imgUrl +
+    '" alt="apple-tv" class="card-img"/>';
+  text += '<h2>' + element.name + '</h2>';
+  text += `<div class="stock"><span class="${
+    element.orderInfo.inStock > 0 ? 'check' : 'close'
+  }"></span><p><b>${element.orderInfo.inStock}
+  </b> left in stock</p></div>`;
+  text += '<p>Price: <b>' + element.price + '</b> $</p>';
+  text += '<div class="addbtn">Add to cart</div> ';
+  text +=
+    ' <div class="card-footer"><div class="col1"><img class="like" src="./img/icons/like_filled.png" alt="like" /><p><b>' +
+    element.orderInfo.reviews +
+    '%</b> Positive reviews</p><p>Above aletage</p>  </div>';
+  text +=
+    '<div class="col2"> <p><b>' +
+    Math.round(Math.random() * (1000 - 300) + 300) +
+    '</b></p> <p>orders</p> </div>  </div></div></div></div> </div>';
+  container.innerHTML += text;
+});
+let cards = document.getElementsByClassName('card');
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', function () {
+    let element = cards[i];
+    console.log(element);
+    let x = document.getElementById('modal');
+    x.style.display = 'flex';
+    let x1 = document.getElementById('modalBack');
+    x1.style.display = 'block';
+    let text = '';
+
+    text +=
+      '<div class ="modal-img"> <img src="./img/' +
+      items[element.id - 1].imgUrl +
+      '" alt=""  /> </div>';
+    text += ' <div class="modal-info"> <h2>' + items[element.id - 1].name + '</h2> ';
+    text +=
+      ' <div class="modal-footer"><div class="col01"><img class="like" src="./img/icons/like_filled.png" alt="like" /><p><b>' +
+      items[element.id - 1].orderInfo.reviews +
+      '%</b> Positive reviews</p><p>Above aletage</p>  </div>';
+    text +=
+      '<div class="col02"> <p><b>' +
+      Math.round(Math.random() * (1000 - 300) + 300) +
+      '</b></p> <p>orders</p> </div>  </div>';
+    text +=
+      '<ul class="info-list"> <li class="info-item">Color: <b> ' +
+      items[element.id - 1].color +
+      '</b></li> <li class="info-item">Operating System:<b>   ' +
+      items[element.id - 1].os +
+      '</b></li> <li class="info-item">Chip:<b>   ' +
+      items[element.id - 1].chip.name +
+      '</b>  </li> <li class="info-item">Height: <b> ' +
+      items[element.id - 1].size.height +
+      ' cm</b></li> <li class="info-item">Width: <b> ' +
+      items[element.id - 1].size.width +
+      ' cm</b></li> <li class="info-item">Depth: <b> ' +
+      items[element.id - 1].size.depth +
+      ' cm</b></li> <li class="info-item">Weight: <b> ' +
+      items[element.id - 1].size.weight +
+      ' g</b></li> </ul>';
+    text +=
+      '</div> <div class="modal-price"><h2>$ ' +
+      items[element.id - 1].price +
+      '</h2> <span class="leftInStock">Stocks:<b> ' +
+      items[element.id - 1].orderInfo.inStock +
+      '</b> pcs.</span> <button class="addbtn">Add to cart</button> </div>';
+    x.innerHTML += text;
   });
 }
 
-// let modal = document.getElementById('modal');
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// };
+document.getElementById('modalBack').addEventListener('click', function () {
+  let x = document.getElementById('modal');
+  x.style.display = 'none';
+  let x1 = document.getElementById('modalBack');
+  x1.style.display = 'none';
+  document.getElementById('modal').innerHTML = '';
+});
